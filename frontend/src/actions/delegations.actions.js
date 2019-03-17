78fbe70312daa2c64cb9beb27922ef0a0bbf1ecc
Bartosz Kudra <bkudra@hotmail.com>
2019-03-18 00:35:36 +0100

    IDEMIA-2019-79 Added web frontend without tests

--- frontend/src/actions/delegations.actions.js
+++ frontend/src/actions/delegations.actions.js
@@ -5,12 +5,10 @@ export const ACTIONS = {
 };
 
 const addNewDelegation = (delegation) => dispatch => {
-  delegation.startDate = delegation.startDate.toISOString();
-  delegation.endDate = delegation.endDate.toISOString();
   return dispatch(
     APIService.post(ACTIONS.POST_DELEGATION, {
       url: '/delegations',
-      //TODO: add authorization token from sessionStorage(?)
+      //TODO: add authorization token from sessionStorage
       headers: {
         "Content-type": "application/json",
         "Bearer": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbXBsb3llZSIsIm5iZiI6MTU1Mjg1Njg4NSwiaXNzIjoiaWRlbWlhIiwiZXhwIjoxNTUyODYwNDg1LCJpYXQiOjE1NTI4NTY4ODUsImF1dGhvcml0aWVzIjpbIlJPTEVfRU1QTE9ZRUUiXX0.PINY8fxa2TKFut7UBf-S08eKCZ7Yo2FolyU6AjwqpfzK3mSRIvlEHJUgejTq86lImFDhm_J5lb34KQbIhaHaEA"
